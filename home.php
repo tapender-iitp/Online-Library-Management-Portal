bc98bfddd9df4f03b3225bcc814ac693ffb67891
Tapender Singh Yadav <tapendersinghyadav92@gmail.com>
2013-12-31 18:53:56 +0530

    Create home.php
    It is the code-snippet for the home page of the project

---
+++ home.php
@@ -0,0 +1,183 @@
+<?php ob_start(); ?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Central Library, IIT Patna</title>
+<link rel="stylesheet" type="text/css" href="header.css"  />
+<link rel="stylesheet" type="text/css" href="heading.css" />
+<link rel="stylesheet" type="text/css" href="body.css"  />
+<link rel="stylesheet" type="text/css" href="navigation.css" />
+<link rel="stylesheet" type="text/css" href="background.css" />
+<link rel="stylesheet" type="text/css" href="akshay.css" /> 
+<script type="text/javascript" src="mscript.js"></script>
+<script type="text/javascript" src="jquery.js"></script>
+<link href="contents.css" rel="stylesheet" type="text/css" />
+<style>
+.wrong
+{
+	position:absolute;
+	left:910px;
+	top:530px;
+	color:red;	
+}
+</style>
+
+
+<script type="text/javascript">
+    window.history.forward();
+    function noBack() { window.history.forward(); }
+</script>
+
+
+<!-- Start WOWSlider.com HEAD section -->
+	<link rel="stylesheet" type="text/css" href="engine1/style.css" media="screen" />
+	<script type="text/javascript" src="engine1/jquery.js"></script>
+<!-- End WOWSlider.com HEAD section -->
+
+</head>
+
+<body class="body" onload="noBack();"
+    onpageshow="if (event.persisted) noBack();" onunload="">
+<div>
+<div class="heading"><h1> INDIAN INSTITUTE OF TECHNOLOGY, PATNA</h1></div>
+<h2 class="header" style="color:#00F; text-decoration:blink;">Central Library Portal</h2>
+<h3 style="color:#00F; text-decoration:blink; margin-left:250px; margin-right:150px"><marquee><img src="ET10_Live_icon.png" /> 24x7 Online Library Portal</marquee></h3>
+<br />
+<hr />
+<!-- Start WOWSlider.com BODY section id=wowslider-container1 -->
+	<div id="wowslider-container1">
+	<div class="ws_images"><ul>
+<li><img src="data1/images/chrysanthemum.jpg" alt="Chrysanthemum" title="Chrysanthemum" id="wows1_0"/></li>
+<li><img src="data1/images/desert.jpg" alt="Desert" title="Desert" id="wows1_1"/></li>
+<li><img src="data1/images/hydrangeas.jpg" alt="Hydrangeas" title="Hydrangeas" id="wows1_2"/></li>
+<li><img src="data1/images/jellyfish.jpg" alt="Jellyfish" title="Jellyfish" id="wows1_3"/></li>
+<li><img src="data1/images/koala.jpg" alt="Koala" title="Koala" id="wows1_4"/></li>
+<li><img src="data1/images/lighthouse.jpg" alt="Lighthouse" title="Lighthouse" id="wows1_5"/></li>
+<li><img src="data1/images/penguins.jpg" alt="Penguins" title="Penguins" id="wows1_6"/></li>
+<li><img src="data1/images/tulips.jpg" alt="Tulips" title="Tulips" id="wows1_7"/></li>
+</ul></div>
+<div class="ws_bullets"><div>
+<a href="#" title="Chrysanthemum"><img src="data1/tooltips/chrysanthemum.jpg" alt="Chrysanthemum"/>1</a>
+<a href="#" title="Desert"><img src="data1/tooltips/desert.jpg" alt="Desert"/>2</a>
+<a href="#" title="Hydrangeas"><img src="data1/tooltips/hydrangeas.jpg" alt="Hydrangeas"/>3</a>
+<a href="#" title="Jellyfish"><img src="data1/tooltips/jellyfish.jpg" alt="Jellyfish"/>4</a>
+<a href="#" title="Koala"><img src="data1/tooltips/koala.jpg" alt="Koala"/>5</a>
+<a href="#" title="Lighthouse"><img src="data1/tooltips/lighthouse.jpg" alt="Lighthouse"/>6</a>
+<a href="#" title="Penguins"><img src="data1/tooltips/penguins.jpg" alt="Penguins"/>7</a>
+<a href="#" title="Tulips"><img src="data1/tooltips/tulips.jpg" alt="Tulips"/>8</a>
+</div></div>
+<a class="wsl" href="http://wowslider.com">Gallery Wordpress Plugin by WOWSlider.com v3.3</a>
+	<a href="#" class="ws_frame"></a>
+	<div class="ws_shadow"></div>
+	</div>
+	<script type="text/javascript" src="engine1/wowslider.js"></script>
+	<script type="text/javascript" src="engine1/script.js"></script>
+<!-- End WOWSlider.com BODY section -->
+<br /><br />
+<ul id="menu"> 
+    <li><a href="x.php">Home</a></li>
+<li><a href="E-Journals.html">E-Journals</a></li>
+<li><a href="E-books.html">E-books</a></li> 
+<li><a href="vendor.html">Vendors</a></li> 
+<li><a href="List of E-Resources_Web Page.pdf" target="new">List of E-Resources</a></li> 
+<li><a href="contact.html">Contact Us</a></li>  
+<li><a href="search.php">Search Books</a></li> 
+</ul> 
+</div>
+<div class="background">
+<div class="contents">
+<br /><br />
+<h1 style="font-style:italic; font-family:">Home</h1><hr />
+<p align=justify>Central Library, IIT Patna was started in Dec 2008 with approximately 1000 books and 1 Online resource. Currently the library contains about 5000 books 15 online resources including E-books and E-journals. The Central Library is equiped with modern technology of library automation system with self check in and check out systems. </p>
+          <p align=justify>The Central Library provides a healthThe Central Library of IIT Patna is one of the advanced libraries in terms of its collection and services. Central Library caters to the information needs of its highly demanding faculty members, students, research scholar as well as staff of the Institute, by offering a wide range of Information Technology (IT) based (and value added) services and products. The Central Library is in fast growing stage. It was started in December 2008 with approximately 1000 books and one online resource. During the short span of time the library has acquired more than 6000 books and about 25 online e-resources including e-books and e-journals. This library is equipped with modern technology of library automation system with Online Public Access Catalogue (OPAC) and Self Check-In and Shelf Check-Out (Drop Box) system using RFID technology. The Library has 15 computers for accessing the different e-resources of the library.
+
+In weekdays the library is kept open between 8.00 AM to 9.00 PM., while Sunday it opens at 9.00AM and closed at 5.00 PM. Users can avail the library facilities during opening hours. Library issues the Institute Identity Card cum Library Card with smart card facility for availing library facility. One can issue or return books at any time in the library during opening hours using the Self Check-In and Shelf Check-Out (Drop Box) system. </p></div>
+          
+<div id="sidebar">
+<div id="logintop">
+<div class="toplogin" id="login1" style="color:#00F;" onclick="switchLogin(1)  ">Admin Login</div>
+<div class="toplogin" id="login2" style="color:#00F;" onclick="switchLogin(2)">Student Login</div>
+</div>
+<div class="middle" id="adminlogin" style="color:#00F;">
+
+<form id="formstyle" method="post" action="x.php" >
+Roll No./Emp Code: <input type="text" name="username" required="required" autofocus="autofocus">
+Password: <input type="password" name="password" required="required">
+<p align="center"><input type="submit" value="Login" id="button"  onclick="alert('You are going to access a high secure page.')><br />
+<p align="center"><a href="forgot.php"><strong style="text-decoration:underline;">Forgot Password ?</strong></a></p>
+</form>
+</div>
+<div class="middle" id="studlogin" style="color:#00F">
+
+<form id="formstyle" method="post" action="studlogin.php">
+Roll No./Emp Code: <input type="text" name="loginid" required="required" autofocus="autofocus">
+Password: <input type="password" name="pwd" required="required">
+<p align="center"><input type="submit" value="Login" id="button" onclick="alert('You are going to access a high secure page.')"></p>
+<p align="center"><a href="#"><strong style="text-decoration:underline;">Forgot Password ?</strong></a></p>
+<script>switchLogin(1);</script>
+</form>
+</div>
+</div>
+<br /><br />
+<div class="middle" style="color:#00F;margin-left:750px;">
+<h3>Important Links</h3>
+<a href="http://www.iitp.ac.in">IIT Patna</a><br /><br />
+<a href="http://www.rbi.org">RBI rates</a><br /><br />
+<a href="http://paniit.iitd.ac.in/indest/">Indest</a><br /><br />
+<a href="impulse_2012.pdf">New Letter - Impulse 2012</a><br /><br />
+<a href="academic_cal_2013.pdf">Academic Calendar</a>
+</div> 
+<div id="footer">
+<footer>
+<?php
+	echo "<div class=\"footer\">";
+	include_once "counter.php";
+?> 
+<br />
+Copyright &copy; Indian Institute of Technology Patna<br>Website designed by Library Management Team</footer>
+</div>
+</div>
+</body>
+<?php 
+//session_start();
+require_once('include.php');
+// Create connection
+$con=mysqli_connect("localhost","abc","abcd","database");
+// Check connection
+if(mysqli_connect_errno($con))
+
+echo "Failed to connect to MySql:" .mysqli_connect_error();
+		
+else
+{		
+if(isset($_POST['username']))
+{
+$txt = $_POST['username'];
+$pwd = $_POST['password'];
+$result = mysqli_query($con,"SELECT * FROM admin WHERE Employee_code='$txt'");
+$result1 = mysqli_query($con,"SELECT * FROM admin WHERE Password='$pwd' and Employee_code='$txt'");
+if($row = mysqli_fetch_array($result))
+{
+	if($row1 = mysqli_fetch_array($result1))
+  	{
+		//session_set_cookie_params(0);
+		session_start();
+		$_SESSION['logged-in'] = true;
+		//$_SESSION['logged-out'] = false;
+		header('Location:admin.php');
+  	}
+  	else
+ 	{
+  		echo "<div class= \"wrong\" ><br /><strong>Invalid Username/Password</strong></div>";
+ 	}
+}
+else
+{
+  	echo "<div class= \"wrong\" ><br /><strong>Invalid Username/Password</strong></div>";
+}
+}
+}
+?>
+
+</html>
